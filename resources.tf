ae1171dfafcb6d67634e5942f1a8479e625fd44a
kalinydimitrov <kalin.ydimitrov@outlook.com>
2025-09-01 15:02:14 +0300

    temp disable S3 backend for state

--- resources.tf
+++ resources.tf
@@ -67,10 +67,11 @@ resource "aws_security_group" "ingress" {
   vpc_id                 = module.main.vpc_id
 }
 
-resource "aws_s3_bucket" "globo-terraform-state" {
-  bucket = "globo-terraform-state"
+# temp disable S3 backend for state
+# resource "aws_s3_bucket" "globo-terraform-state" {
+#   bucket = "globo-terraform-state"
 
 
-  tags = local.common_tags
+#   tags = local.common_tags
 
-}
+# }
