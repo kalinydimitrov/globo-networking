7dd9d593bd824dc77ebc0656b4c47fc5d3e9d5ab
kalinydimitrov <kalin.ydimitrov@outlook.com>
2025-09-01 14:45:46 +0300

    switching to local tf state

--- terraform.tf
+++ terraform.tf
@@ -6,14 +6,20 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket = "globo-terraform-state"
-    key    = "network_config/terraform.tfstate"
-    region = "us-east-1"
-    # dynamodb_table = "globo-terraform-locks"
-    use_lockfile = true
-    encrypt      = true
-
+  ### used only for the fist run to create the S3 bucket - comment bellow to use S3 as backend
+  backend "local" {
+    path = "terraform.tfstate"
   }
+
+  ### uncomment bellow to use S3 as backend - AFTER CREATING THE BUCKET with the first run
+  # backend "s3" {
+  #   bucket = "globo-terraform-state"
+  #   key    = "network_config/terraform.tfstate"
+  #   region = "us-east-1"
+  #   # dynamodb_table = "globo-terraform-locks"
+  #   use_lockfile = true
+  #   encrypt      = true
+
+  # }
 
 }
