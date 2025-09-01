b543f81eca097d71571e7385f04d319505502ec8
kalinydimitrov <kalin.ydimitrov@outlook.com>
2025-09-01 19:44:28 +0300

    blabla 3

--- terraform.tf
+++ terraform.tf
@@ -7,19 +7,19 @@ terraform {
   }
 
   ### used only for the fist run to create the S3 bucket - comment bellow to use S3 as backend
-  backend "local" {
-    path = "terraform.tfstate"
-  }
+  # backend "local" {
+  #   path = "terraform.tfstate"
+  # }
 
   ### uncomment bellow to use S3 as backend - AFTER CREATING THE BUCKET with the first run
-  # backend "s3" {
-  #   bucket = "globo-terraform-state"
-  #   key    = "network_config/terraform.tfstate"
-  #   region = "us-east-1"
-  #   # dynamodb_table = "globo-terraform-locks"
-  #   use_lockfile = true
-  #   encrypt      = true
+  backend "s3" {
+    bucket = "globo-terraform-state"
+    key    = "network_config/terraform.tfstate"
+    region = "us-east-1"
+    # dynamodb_table = "globo-terraform-locks"
+    use_lockfile = true
+    encrypt      = true
 
-  # }
+  }
 
 }
