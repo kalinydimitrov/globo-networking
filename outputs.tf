fff9f06f427e1aa7a9dcb85219e031f489c0b821
kalinydimitrov <kalin.ydimitrov@outlook.com>
2025-09-01 16:35:45 +0300

    adding new outputs 2

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 ##################################################################################
-# OUTPUT
+# OUTPUT VARIABLES
 ##################################################################################
 
 output "vpc_id" {
   description = "The ID of the VPC"
   value       = module.main.vpc_id
-  
+
 }
 
 output "public_subnets" {
   description = "List of public subnet IDs"
   value       = module.main.public_subnets
-  
+
 }
 
